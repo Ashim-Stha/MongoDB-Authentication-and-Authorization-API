ae37553ab2f4ebdba528afb23d3ff8be73b55b11
Ashim-Stha <ashimshrestha2384@gmail.com>
2024-05-19 12:05:03 +0545

    modified auth controller

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -8,7 +8,7 @@ const handleLogout = async (req, res) => {
   const refreshToken = cookies.jwt;
 
   // Is refreshToken in db?
-  const foundUser = await User.findOne({ refreshToken });
+  const foundUser = await User.findOne({ refreshToken }).exec();
   if (!foundUser) {
     res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
     return res.sendStatus(204);
